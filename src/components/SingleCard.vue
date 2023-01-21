fb9785409716ba508f330f0421cfb3a622f35a38
MatteoGenovese <matteo.genovese@icloud.com>
2023-01-21 18:33:10 +0100

    fix: fade animation

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,14 +1,14 @@
 <template>
-<div v-if="work.visible" class="wrapper" data-aos="fade-in">
+<div v-if="work.visible" class="wrapper" 
+    @click="cardToggle()"
+    :class="isFrontActive ? 'active' : 'back'">
         <div class="card h-100">
 
-            <div class="card__front" 
-                    @click="cardToggle()" :class="isFrontActive ? 'active' : ''">
+            <div class="card__front"  >
                 <img :src="require(`../assets/works/${work.imageName}`)" :alt="work.imageName" class="img-fluid">
             </div>
 
-            <div class="card__back d-flex flex-column justify-content-between"
-                    @click="cardToggle()" :class="!isFrontActive ? 'active' : ''">
+            <div class="card__back d-flex flex-column justify-content-between">
                 <div class="content">
                     <h2>{{ work.title }}</h2>
                     <p v-html="work.description"></p>
@@ -68,89 +68,109 @@ export default {
 @import '../sass/app.scss';
 
 
-.wrapper,
-.card {
-  height: 50rem;
-  
+.wrapper{
+    height: 50rem;
+    perspective: 90rem;
+
+    .card {
+        box-shadow: 2rem -2rem 2rem rgba(0, 0, 0, 0.255);
+        -moz-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
+        -webkit-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
+        -o-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
+        
+        height: 100%;
+        text-align: center;
+        transition: all 0.75s cubic-bezier(0.7, -.5, 0.3, 1.8);
+        transform-style: preserve-3d;
+        border-radius: 0.5rem;
+        position: relative;
+
+
+        &__front {
+            overflow: hidden;
+            cursor: pointer;
+            backface-visibility: hidden;
+            background-size: contain;
+            background-position: center center;
+            background-color: transparent;
+        }
+
+        &__back {
+            overflow: scroll;
+            padding: 4rem 4rem;
+            position: absolute;
+            background-color: $primary_color_light;
+            .content {
+                h2 {
+                    font-size: 3.5rem;
+                    margin-bottom: 2rem;
+                    font-weight: 300;
+                }
+                p {
+                    font-size: $default-font-size;
+                    margin: 1rem 0;
+                    font-weight: 300;
+                    text-align: justify;
+                    text-justify: inter-word;
+                }
+                h4 {
+                margin: 0;
+                }
+            }
+            // top: 100%;
+        }
+
+        &__front,
+        &__back {
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            border-radius: 0.5rem;
+            top: 0;
+            left: 0;
+            backface-visibility: hidden;
+            img {
+                width: 100%;
+                object-fit: contain;
+            }
+        }
+    }
 }
 
-.wrapper:hover .card, .card__front.active {
-  transform: rotateY(180deg);
+
+@media (hover: hover)
+{
+    .wrapper:hover .card {
+        transform: rotateY(180deg);
+    }
+
+
+
+    .card__back {
+        transform: rotateY(180deg);
+        position: absolute;
+    }
+
+
 }
 
-.wrapper {
-  perspective: 90rem;
+@media (hover: none)
+{
+
+    .wrapper.back .card {
+        transform: rotateY(180deg);
+    }
+
+
+
+    .card__back {
+        transform: rotateY(180deg);
+        position: absolute;
+    }
+    
 }
 
 
-.card {
-  text-align: center;
-  transition: all 0.75s cubic-bezier(0.7, -.5, 0.3, 1.8);
-  transform-style: preserve-3d;
-  box-shadow: 2rem -2rem 2rem rgba(0, 0, 0, 0.255);
-  -moz-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
-  -webkit-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
-  -o-box-shadow: 1rem -1rem 0.5rem rgba(0, 0, 0, 0.255);
-  border-radius: 0.5rem;
-  position: relative;
-
-
-  &__front.active {
-      cursor: pointer;
-      backface-visibility: hidden;
-      background-size: contain;
-      background-position: center center;
-      background-color: transparent;
-
-
-  }
-
-  &__back.active {
-      transform: rotateY(180deg);
-      position: absolute;
-      background-color: $primary_color_light;
-
-
-      .content {
-          h2 {
-              font-size: 3.5rem;
-              margin-bottom: 2rem;
-              font-weight: 300;
-          }
-          p {
-              font-size: $default-font-size;
-              margin: 1rem 0;
-              font-weight: 300;
-              text-align: justify;
-              text-justify: inter-word;
-          }
-          h4 {
-          margin: 0;
-      }
-      }
-
-
-  }
-  &__back {
-    padding: 4rem 4rem;
-  }
-
-  &__front,
-  &__back {
-      height: 100%;
-      width: 100%;
-      overflow: hidden;
-      position: absolute;
-      border-radius: 0.5rem;
-      top: 0;
-      left: 0;
-      backface-visibility: hidden;
-      img {
-          width: 100%;
-          object-fit: contain;
-      }
-  }
-}
 
 .link {
   .tags {
